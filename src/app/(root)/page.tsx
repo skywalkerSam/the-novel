98f3ae3c4119e94917c9176e88be078e4153355b
Starboy <samskywalker001@gmail.com>
2025-05-19 13:38:23 +0530

    landing page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,12 @@
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-sky-950 text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-transparent via-sky-400 to-transparent text-white">
       <div className="container flex flex-col items-center justify-center">
         <p className="text-2xl font-extrabold tracking-tight md:text-[2rem]">
-          <span className="text-sky-200">Welcome</span> to
+          <span className="text-black font-extrabold">Welcome</span> to
         </p>
         <h1 className="text-5xl font-extrabold tracking-tight text-white md:text-[5rem]">
-          The <span className="text-sky-200">Novel</span>
+          The <span className="text-black">Novel</span>
         </h1>
       </div>
     </main>
