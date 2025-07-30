df048941d503a218c4c89764ff3e0672117b18e2
Starboy <samskywalker001@gmail.com>
2025-07-30 15:30:14 +0000

    #344532

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-transparent via-sky-400 to-transparent text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-transparent via-[var(--green)] to-transparent text-white">
       <div className="container flex flex-col items-center justify-center">
         <p className="text-2xl font-extrabold tracking-tight md:text-[2rem]">
           <span className="font-extrabold text-black">
