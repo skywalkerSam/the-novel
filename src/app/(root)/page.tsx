7b0987cd00e640c07dc55c05f0e152da941ad8bd
Starboy <samskywalker001@gmail.com>
2025-08-20 03:01:32 +0000

    hotfix

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,14 +3,14 @@ export default function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-transparent via-[var(--green)] to-transparent">
       <div className="container flex flex-col items-center justify-center">
         <p className="text-2xl font-extrabold tracking-tight md:text-[2rem] text-gray-300">
-          <span className="text-green-950">
+          <span className="text-black">
             <strong>Welcome</strong>
           </span>{" "}
           to
         </p>
         <h1 className="text-5xl font-extrabold tracking-tight text-gray-300 md:text-[5rem]">
           The{" "}
-          <span className="text-green-950">
+          <span className="text-black">
             <strong>Novel</strong>
           </span>
         </h1>
