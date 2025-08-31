7ad924ba81203ce3ee4fc3f54911af550a16f232
Starboy <samskywalker001@gmail.com>
2025-08-31 11:19:36 +0000

    layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,9 @@ import "~/styles/globals.css";
 
 import { type Metadata } from "next";
 import { Paprika } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+// import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "~/components/ui/theme-provider";
 import Footer from "~/components/Footer";
-// import {
-//   ClerkProvider,
-//   SignedIn,
-//   SignInButton,
-//   SignedOut,
-// } from "@clerk/nextjs";
-
-// import NavBar from "./_components/NavBar";
 
 // https://nextjs.org/learn/dashboard-app/adding-metadata
 export const metadata: Metadata = {
@@ -35,7 +27,6 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    // <ClerkProvider>
     <html lang="en" suppressHydrationWarning>
       <body className={`${paprika.className} antialiased`}>
         <ThemeProvider
@@ -44,22 +35,12 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <header className="flex h-16 items-center justify-end gap-4 p-4">
-            {/* <SignedOut>
-                <div className="px-4 py-2 hover:rounded-full hover:text-primary">
-                  <SignInButton />
-                </div>
-              </SignedOut>
-              <SignedIn>
-                <NavBar></NavBar>
-              </SignedIn> */}
-          </header>
+          <header className="flex h-16 items-center justify-end gap-4 p-4"></header>
           <main>{children}</main>
           <Footer></Footer>
         </ThemeProvider>
-        <SpeedInsights></SpeedInsights>
+        {/* <SpeedInsights></SpeedInsights> */}
       </body>
     </html>
-    // </ClerkProvider>
   );
 }
