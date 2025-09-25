8645064ce0fbed0bbd222262014b4989626c52f8
Starboy <samskywalker001@gmail.com>
2025-09-25 16:14:31 +0000

    icons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -44,23 +44,53 @@ export default function NavBar() {
                     className="hover:text-primary block rounded-sm px-3 py-2 md:bg-transparent md:p-0"
                     aria-current="page"
                   >
-                    Home
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                      />
+                    </svg>
+
+                    {/* Home */}
                   </Link>
                 </li>
-                <li>
+                {/* <li>
                   <Link
                     href="#"
                     className="hover:text-primary block rounded-sm px-3 py-2 md:p-0"
                   >
                     Community
                   </Link>
-                </li>
+                </li> */}
                 <li>
                   <Link
                     href="/search"
                     className="hover:text-primary block rounded-sm px-3 py-2 md:p-0"
                   >
-                    Search
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                      />
+                    </svg>
+
+                    {/* Search */}
                   </Link>
                 </li>
               </ul>
